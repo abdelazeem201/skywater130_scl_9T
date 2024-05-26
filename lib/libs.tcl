09e28bbdf8fe5745b7e3341b2da70db001490076
Ahmed Abdelazeem <58098260+abdelazeem201@users.noreply.github.com>
2024-05-26 10:20:31 +0300

    libs.tcl

---
+++ lib/libs.tcl
@@ -0,0 +1,12 @@
+read_lib sky130_ff_1.98_0.lib
+write_lib sky130_ff_1.98_0 -output sky130_ff_1.98_0.db
+
+read_lib sky130_ss_1.62_125.lib
+write_lib sky130_ss_1.62_125 -output sky130_ss_1.62_125.db
+
+read_lib sky130_tt_1.8_25.lib
+write_lib sky130_tt_1.8_25 -output sky130_tt_1.8_25.db
+
+
+
+exit
